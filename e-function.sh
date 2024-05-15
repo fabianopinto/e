d6297ec40aaaaa108d19ec72d25aadac8c7f262d
Fabiano Pinto <fabiano.pinto@philips.com>
2024-05-15 13:38:24 +0200

    Add help, list, and dump options

--- e-function.sh
+++ e-function.sh
@@ -1,31 +1,60 @@
 #!/bin/bash
 
 function e {
-  for i in $__e; do
-    unset "$i"
-  done
-  __e=()
-  local old_pwd="$PWD"
-  while [ "$1" != "" ]; do
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: e [OPTION] [ENVIRONMENT]...
+Handles multiple environment setups.
+
+  -h, --help    Display this help and exit
+  -l, --list    List available environments
+  -d, --dump    Dump the current environment
+  ENVIRONMENT   Load the specified environment
+
+Check https://github.com/fabianopinto/e"
+  elif [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
+    local old_pwd="$PWD"
     while [ "$PWD" != "/" ]; do
-      if [ -f ".local/$1.env" ]; then
-        if [[ "$(stat -c %A ".local/$1.env")" =~ ^...x.-..-.$ ]]; then
-          while IFS="" read -r line; do
-            __e+=("$line")
-          done < <(sed -E "s/^(export ([^=]+))?.*/\2/" ".local/$1.env")
-          # shellcheck disable=SC1090
-          source ".local/$1.env"
-        else
-          echo "Security risk ($(stat -c %A ".local/$1.env")): .local/$1.env" >&2
-        fi
-        break
+      if [ -d ".local" ]; then
+        find .local -name "?*.env" -maxdepth 1 | sort | sed "s/^\.local\///; s/\.env$//"
       fi
       cd ..
-      if [ "$PWD" = "/" ]; then
-        echo "Environment not found: .local/$1.env" >&2
+    done
+    cd "$old_pwd" || true
+  elif [ "$1" = "-d" ] || [ "$1" = "--dump" ]; then
+    for i in $__e; do
+      if [[ "$i" =~ (PASSWORD|PASSWD|PWD) ]]; then
+        echo "export $i='****'"
+      else
+        eval "echo \"export $i='\$$i'\""
       fi
     done
-    shift
-    cd "$old_pwd"
-  done
+  else
+    for i in $__e; do
+      unset "$i"
+    done
+    __e=()
+    local old_pwd="$PWD"
+    while [ "$1" != "" ]; do
+      while [ "$PWD" != "/" ]; do
+        if [ -f ".local/$1.env" ]; then
+          if [[ "$(stat -c %A ".local/$1.env")" =~ ^...x.-..-.$ ]]; then
+            while IFS="" read -r line; do
+              __e+=("$line")
+            done < <(sed -E "s/^(export ([^=]+))?.*/\2/" ".local/$1.env")
+            # shellcheck disable=SC1090
+            source ".local/$1.env"
+          else
+            echo "Security risk ($(stat -c %A ".local/$1.env")): .local/$1.env" >&2
+          fi
+          break
+        fi
+        cd ..
+        if [ "$PWD" = "/" ]; then
+          echo "Environment not found: .local/$1.env" >&2
+        fi
+      done
+      shift
+      cd "$old_pwd" || true
+    done
+  fi
 }
